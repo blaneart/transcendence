fd446358ae9d6b0f30204b6552268b518c35cb49
hcanon42 <hcanon@student.42.fr>
2021-09-09 14:55:47 +0100

    launch.sh also works with xterm and konsole

--- launcher.sh
+++ launcher.sh
@@ -1,15 +1,31 @@
+
+if command -v qterminal &> /dev/null
+then
+	terminal_used=qterminal
+fi
+
+if command -v xterm &> /dev/null
+then
+	terminal_used=xterm
+fi
+
+if command -v konsole &> /dev/null
+then
+	terminal_used=konsole
+fi
+
 printf '\nLaunching DataBase...\n\n'
 
 cd ./database
 sudo docker-compose down
-qterminal -e "sudo docker-compose up"&
+$terminal_used -e "sudo docker-compose up"&
 printf '\n\nLaunching Backend...\n\n'
 
 sleep 10
 cd ../pong-backend
-qterminal -e "npm run start:dev"&
+$terminal_used -e "npm run start:dev"&
 printf '\n\nLaunching Frontend...\n\n'
 
 sleep 10
 cd ../front-typescript
-qterminal -e "npm start"
+$terminal_used -e "npm start"

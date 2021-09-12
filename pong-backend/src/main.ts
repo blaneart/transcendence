a5841fbb5d8a6dd17b4d5887f08811949da4f377
Arseny Shishaev <ashishae@student.42.fr>
2021-09-13 00:01:47 +0100

    Add global validation, parse ints in profile

--- pong-backend/src/main.ts
+++ pong-backend/src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as session from 'express-session';
+import { ValidationPipe } from '@nestjs/common';
 
 const APP_SECRET: string = process.env.APP_SECRET;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  app.useGlobalPipes(new ValidationPipe());
   app.use(
     session({
       secret: APP_SECRET,
--- pong-backend/src/profile/profile.controller.ts
+++ pong-backend/src/profile/profile.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Res, HttpStatus, Param, Req, UseGuards, Request } from '@nestjs/common';
+import { Controller, Get, Post, Body, Res, HttpStatus, Param, Req, UseGuards, Request, ParseIntPipe } from '@nestjs/common';
 import { ProfileService } from './profile.service';
 import { AchievementService } from '../achievement/achievement.service';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
@@ -15,22 +15,21 @@ export class ProfileController {
     @Get('/me') // returns 'my' profile
     async getProfile(@Request() req) {
       // Gets the profile that corresponds to the id saved in session
-      console.log('me');
       return this.profileService.getUserById(req.user.id);
     }
 
     @UseGuards(JwtAuthGuard) // only for logged in
     @Get(':id/achievements')
-    async userAchievements(@Param('id') id: number)
+    async userAchievements(@Param('id', ParseIntPipe) id: number)
     {
       // Return all achievements that belong to this user
       return this.achievementService.getAchievementsByUserId(id);
     }
 
     @Get(':id')
-    show(@Param('id') id: number)
+    async show(@Param('id', ParseIntPipe) id: number)
     {
-        return this.profileService.getUserById(id);
+      return await this.profileService.getUserById(id);
     }
 }
 

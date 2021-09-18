8760cfb8e14e1fa8a5e38ffbe4e44721c8161ac9
blaneart <blanar.a.v@gmail.com>
2021-09-18 23:13:08 +0200

    games

--- pong-backend/src/game/game.gateway.ts
+++ pong-backend/src/game/game.gateway.ts
@@ -188,13 +188,25 @@ export class GameGateway implements OnGatewayInit {
           if (this.rooms[roomName].players[0].id === 0)
             playerid = 0;
           if (this.rooms[roomName].scores[0] >= 10)
-            this.gameService.saveGame(this.rooms[roomName].players[playerid].userId, this.rooms[roomName].players[1 - playerid].userId,  this.rooms[roomName].scores[1])
+          {
+            this.gameService.saveGame(this.rooms[roomName].players[playerid].userId, 
+              this.rooms[roomName].players[1 - playerid].userId,  this.rooms[roomName].scores[1]);
+
+            this.server.to(roomName).emit('endGame', this.rooms[roomName].players[playerid].name);
+          }
           else
-            this.gameService.saveGame(this.rooms[roomName].players[1 - playerid].userId, this.rooms[roomName].players[playerid].userId,  this.rooms[roomName].scores[0])
+          {
+            this.gameService.saveGame(this.rooms[roomName].players[1 - playerid].userId,
+                this.rooms[roomName].players[playerid].userId,  this.rooms[roomName].scores[0]);
+
+            this.server.to(roomName).emit('endGame', this.rooms[roomName].players[1 - playerid].name);
+
+
+          }
 
           this.server.emit('changeScore', this.rooms[roomName].scores)
 
-          this.server.to(roomName).emit('endGame');
+
           clearInterval(interval);
         }
 
@@ -214,9 +226,12 @@ export class GameGateway implements OnGatewayInit {
   @SubscribeMessage('watchMatch')
   watchMatch(client: Socket, roomName: string)
   {
-    console.log(roomName);
     client.join(roomName)
-    console.log(this.server.sockets.adapter.rooms.get(roomName).size)
+    let playerid = 1;
+    if (this.rooms[roomName].players[0].id === 0)
+      playerid = 0;
+    this.server.to(client.id).emit('playersNames', this.rooms[roomName].players[playerid].name,
+                            this.rooms[roomName].players[1 - playerid].name)
   }
 
   @SubscribeMessage('quitGame')
@@ -294,6 +309,9 @@ export class GameGateway implements OnGatewayInit {
   }
 
 
+  @SubscribeMessage('')
+
+  
   @SubscribeMessage('joinRoom')
   createRoom(socket: Socket, userInfo) {
     console.log('joinRoom');
